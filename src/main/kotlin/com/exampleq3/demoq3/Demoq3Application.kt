a726c80fd2f7fe5b5a9d61d3ae201c67339633bc
NEON <mgutie33@hotmail.com>
2022-11-02 19:48:34 -0500

    first commit

---
+++ src/main/kotlin/com/exampleq3/demoq3/Demoq3Application.kt
@@ -0,0 +1,11 @@
+package com.exampleq3.demoq3
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Demoq3Application
+
+fun main(args: Array<String>) {
+	runApplication<Demoq3Application>(*args)
+}
---
+++ src/test/kotlin/com/exampleq3/demoq3/Demoq3ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.exampleq3.demoq3
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Demoq3ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
